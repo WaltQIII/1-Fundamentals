0fb84254695530a102aa9b2707eb2d85c6e1626d
wquar <wquarles25@gmail.com>
2022-09-06 01:01:30 -0500

    Python Fundamentals

---
+++ cc1.py
@@ -0,0 +1,17 @@
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 4.0
+print(type(b))
+
+c = "Walt"
+print(type("Walt"))
+
+d = a + b
+print(d)
+
+
